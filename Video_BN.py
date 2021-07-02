43337fd4d3500b314e2ce11fb6d0f85bc2905bbb
Pablor1409 <66320139+Pablor1409@users.noreply.github.com>
2021-07-02 08:32:27 +0200

    Add files via upload

---
+++ Video_BN.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(0)
+
+# Check if camera opened successfully
+if (cap.isOpened()== False):
+  print("Error opening video stream or file")
+
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  framegr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+  if ret == True:
+
+    # Display the resulting frame
+    cv2.imshow('Frame',framegr)
+
+
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord("q"):
+      break
+
+  # Break the loop
+  else:
+    break
